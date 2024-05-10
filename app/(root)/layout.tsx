877510f481ed459317811e091af776b705ed0830
Alvin Ronald <alvinronald123@gmail.com>
2024-05-10 15:53:54 -0700

    neee

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import MobileNav from "@/components/ui/MobileNav";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { Sidebar } from "lucide-react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const loggedIn = { firstName: 'Alvin', lastName: 'JSM' };
+    return (
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn} />
+
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+                    <div>
+                        <MobileNav user={loggedIn} />
+                    </div>
+                </div>
+                {children}
+            </div>
+        </main>
+    );
+}
